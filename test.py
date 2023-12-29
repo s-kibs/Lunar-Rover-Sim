4fdb1b208a6e13a73e52c206fb9274b7aadb2178
s.kibs <49878108+s-kibs@users.noreply.github.com>
2023-12-29 19:53:11 +0530

    Digit Recognition system In depth learning of Dimension reduction, flatenning

--- test.py
+++ test.py
@@ -1,25 +1,32 @@
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
+mnist = tf.keras.datasets.mnist
 
-# Generate random data
-np.random.seed(0)
-X = np.random.rand(100, 1)
-Y = 2 * X + 1 + 0.1 * np.random.rand(100, 1)
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
-# Define the model
-model = tf.keras.Sequential([tf.keras.layers.Dense(units=1, input_shape=[1])])
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
 
-# Compile the model
-model.compile(optimizer='sgd', loss='mean_squared_error')
+# from tensorflow.keras.losses import SparseCategoricalCrossentropy
+# model.compile(loss=SparsCategoricalCrossentropy)
 
-# Train the model
-model.fit(X, Y, epochs=100)
-
-# Make predictions
-predictions = model.predict(X)
-
-# Plot the results
-plt.scatter(X, Y)
-plt.plot(X, predictions, color='red')
-plt.show()
+predictions = model(x_train[:1]).numpy()
+predictions
+tf.nn.softmax(predictions).numpy()
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+loss_fn(y_train[:1], predictions).numpy()
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test,  y_test, verbose=2)
+probability_model = tf.keras.Sequential([
+  model,
+  tf.keras.layers.Softmax()
+])
+probability_model(x_test[:5])
